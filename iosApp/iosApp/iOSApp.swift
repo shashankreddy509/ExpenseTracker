863a468b5941c9942492e2ef615b910c36459612
Shashank Reddy Ganta <shashankreddy509@gmail.com>
2025-04-26 20:59:40 -0500

    Added google plist file and updated to support firebase integration.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,11 @@
 import SwiftUI
+import FirebaseCore
 
 @main
 struct iOSApp: App {
+    init() {
+       FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
